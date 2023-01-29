1a9dc493585be329acd0ff9db8f408a97bb1a848
bunn1 <nicjohbun@gmail.com>
2023-01-29 12:09:12 +0100

    outcomment code, renaming footer content

--- wp-content/themes/flowerpower/footer.php
+++ wp-content/themes/flowerpower/footer.php
@@ -9,7 +9,7 @@
     <div class="container">
         <div class="row">
 
-        <div class="col-lg-3 col-md-6">
+        <!-- <div class="col-lg-3 col-md-6">
             <div class="footer-info">
             <h3>HeroBiz</h3>
             <p>
@@ -19,36 +19,42 @@
                 <strong>Email:</strong> info@example.com<br>
             </p>
             </div>
-        </div>
+        </div> -->
 
         <div class="col-lg-2 col-md-6 footer-links">
-            <h4>Useful Links</h4>
+            <h4>Kund Service</h4>
             <ul>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Home</a></li>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">About us</a></li>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Services</a></li>
+            <li><i class="bi bi-chevron-right"></i> <a href="#">Returer</a></li>
+            <li><i class="bi bi-chevron-right"></i> <a href="#">Villkor</a></li>
+            <!-- <li><i class="bi bi-chevron-right"></i> <a href="#">Services</a></li>
             <li><i class="bi bi-chevron-right"></i> <a href="#">Terms of service</a></li>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Privacy policy</a></li>
+            <li><i class="bi bi-chevron-right"></i> <a href="#">Privacy policy</a></li> -->
             </ul>
         </div>
 
         <div class="col-lg-3 col-md-6 footer-links">
-            <h4>Our Services</h4>
+            <h4>Företaget</h4>
             <ul>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Web Design</a></li>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Web Development</a></li>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Product Management</a></li>
+            <li><i class="bi bi-chevron-right"></i> <a href="#">Om Oss</a></li>
+            <li><i class="bi bi-chevron-right"></i> <a href="#">Frågor & Svar</a></li>
+            <!-- <li><i class="bi bi-chevron-right"></i> <a href="#">Product Management</a></li>
             <li><i class="bi bi-chevron-right"></i> <a href="#">Marketing</a></li>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Graphic Design</a></li>
+            <li><i class="bi bi-chevron-right"></i> <a href="#">Graphic Design</a></li> -->
             </ul>
         </div>
 
         <div class="col-lg-4 col-md-6 footer-newsletter">
-            <h4>Our Newsletter</h4>
-            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
-            <form action="" method="post">
+            <h4>Social Media</h4>
+            <!-- <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p> -->
+
+            <div class="social-links order-first order-lg-last mb-3 mb-lg-0">
+        <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+        </div>
+            <!-- <form action="" method="post">
             <input type="email" name="email"><input type="submit" value="Subscribe">
-            </form>
+            </form> -->
 
         </div>
 
@@ -61,24 +67,18 @@
 
         <div class="d-flex flex-column align-items-center align-items-lg-start">
         <div class="copyright">
-            &copy; Copyright <strong><span>HeroBiz</span></strong>. All Rights Reserved
+            &copy; Copyright <strong><span>2023 Florera</span></strong>. All Rights Reserved
         </div>
-        <div class="credits">
+        <!-- <div class="credits"> -->
             <!-- All the links in the footer should remain intact. -->
             <!-- You can delete the links only if you purchased the pro version. -->
             <!-- Licensing information: https://bootstrapmade.com/license/ -->
             <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/herobiz-bootstrap-business-template/ -->
-            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-        </div>
+            <!-- Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        </div> -->
         </div>
 
-        <div class="social-links order-first order-lg-last mb-3 mb-lg-0">
-        <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-        <a href="#" class="google-plus"><i class="bi bi-skype"></i></a>
-        <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
-        </div>
+      
 
     </div>
     </div>
