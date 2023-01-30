4b1a291f720c324318ee3a04f6dc5d5ef742fb7c
bunn1 <nicjohbun@gmail.com>
2023-01-30 20:21:54 +0100

    created h1 created wp_nav_menu() commenting out code

--- wp-content/themes/flowerpower/header.php
+++ wp-content/themes/flowerpower/header.php
@@ -7,45 +7,66 @@
     <head>
         <meta charset="<?php bloginfo( 'charset' ) ?>">
         <meta name="viewport" content="width=device-width, intial-scale=1">
+      
         <?php wp_head(); ?>
     </head>
+
     <body <?php body_class(); ?>>
+
+    
         <!-- ======= Header ======= -->
         <header id="header" class="header fixed-top" data-scrollto-offset="0">
+
             <div class="container-fluid d-flex align-items-center justify-content-between">
 
-            <?php
-                $custom_logo_id = get_theme_mod( 'custom_logo' );
-                $logo = wp_get_attachment_image_src( $custom_logo_id, 'full' );
+            <h1>Florera</h1>
 
-                if ( has_custom_logo() ) {
-                    printf(
-                        '<a href="%1$s" class="logo d-flex align-items-center scrollto me-auto me-lg-0"><img src="%2$s"/></a>',
-                        esc_url( home_url() ),
-                        esc_url( $logo[0] )
-                    );
-                } else {
-                    echo bloginfo( 'name' );
-                }
+              <!-- 1 . header menu -->
+    
+        <?php wp_nav_menu (
+                    
+                    array(
+                            'theme_location'  =>  'top-menu',
+                            'theme_class'  =>  'navigation'
+                    )
+            ); ?>
+
+            <!-- 1. Get in touch with style.css  / Grabs only header.php page -->
+            <?php 
+            //  echo "<link rel='stylesheet' type='text/css' href='/wp-content/themes/flowerpower/style.css' />"; 
+            ?>
+            <?php
+                // $custom_logo_id = get_theme_mod( 'custom_logo' );
+                // $logo = wp_get_attachment_image_src( $custom_logo_id, 'full' );
+
+                // if ( has_custom_logo() ) {
+                //     printf(
+                //         '<a href="%1$s" class="logo d-flex align-items-center scrollto me-auto me-lg-0"><img src="%2$s"/></a>',
+                //         esc_url( home_url() ),
+                //         esc_url( $logo[0] )
+                //     );
+                // } else {
+                //     echo bloginfo( 'name' );
+                // }
 
             ?>
             <nav id="navbar" class="navbar">
                 <?php
-                    if ( has_nav_menu( 'primary' ) ) :
-                        wp_nav_menu( [
-                            'theme_location' => 'primary',
-                            'container'      => false,
-                            'menu_class'     => '',
-                            'menu_id'        => '',
-                            'depth'          => 3
-                        ] );
-                    else :
-                        printf(
-                            '<a href="%1$s">%2$s</a>',
-                            esc_url( admin_url( '/nav-menus.php' ) ),
-                            esc_html__( 'Asign a menu', 'herobiz' )
-                        );
-                    endif;
+                    // if ( has_nav_menu( 'primary' ) ) :
+                    //     wp_nav_menu( [
+                    //         'theme_location' => 'primary',
+                    //         'container'      => false,
+                    //         'menu_class'     => '',
+                    //         'menu_id'        => '',
+                    //         'depth'          => 3
+                    //     ] );
+                    // else :
+                    //     printf(
+                    //         '<a href="%1$s">%2$s</a>',
+                    //         esc_url( admin_url( '/nav-menus.php' ) ),
+                    //         esc_html__( 'Asign a menu', 'herobiz' )
+                    //     );
+                    // endif;
                 ?>
                 <!-- <ul>
                 <li><a class="nav-link scrollto" href="index.html#hero">Home</a></li>
@@ -77,21 +98,21 @@
             </nav><!-- .navbar -->
 
             <?php
-                if ( has_nav_menu( 'header_action' ) ) :
-                    wp_nav_menu( [
-                        'theme_location' => 'header_action',
-                        'container'      => false,
-                        'menu_class'     => 'header_menu_action',
-                        'menu_id'        => '',
-                        'depth'          => 3
-                    ] );
-                else :
-                    printf(
-                        '<a class="btn-getstarted scrollto" href="%1$s">%2$s</a>',
-                        esc_url( admin_url( '/nav-menus.php' ) ),
-                        esc_html__( 'Action Menu', 'herobiz' )
-                    );
-                endif;
+                // if ( has_nav_menu( 'header_action' ) ) :
+                //     wp_nav_menu( [
+                //         'theme_location' => 'header_action',
+                //         'container'      => false,
+                //         'menu_class'     => 'header_menu_action',
+                //         'menu_id'        => '',
+                //         'depth'          => 3
+                //     ] );
+                // else :
+                //     printf(
+                //         '<a class="btn-getstarted scrollto" href="%1$s">%2$s</a>',
+                //         esc_url( admin_url( '/nav-menus.php' ) ),
+                //         esc_html__( 'Action Menu', 'herobiz' )
+                //     );
+                // endif;
             ?>
 
             </div>
