ea8fb495eb0c99a003bcba6dfeb06e1390e5be0c
JohnSommerOsterlin <90193135+JohnSommerOsterlin@users.noreply.github.com>
2023-02-07 14:11:14 +0100

    removed footer legal container

--- wp-content/themes/flowerpower/footer.php
+++ wp-content/themes/flowerpower/footer.php
@@ -53,7 +53,7 @@
         </div> -->
     </div>
 
-    <div class="footer-legal text-center">
+    <!-- <div class="footer-legal text-center">
         <div
             class="container d-flex flex-column flex-lg-row justify-content-center justify-content-lg-between align-items-center">
 
@@ -61,14 +61,14 @@
                 <div class="copyright">
                     &copy; Copyright <strong><span>Florera</span></strong>. All Rights Reserved
                 </div>
-                <!-- <div class="credits"> -->
+                <div class="credits"> -->
                     <!-- All the links in the footer should remain intact. -->
                     <!-- You can delete the links only if you purchased the pro version. -->
                     <!-- Licensing information: https://bootstrapmade.com/license/ -->
                     <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/herobiz-bootstrap-business-template/ -->
                     <!-- Designed by <a href="https://bootstrapmade.com/"></a> -->
                 <!-- </div> -->
-            </div>
+            <!-- </div> -->
 <!-- 
             <div class="social-links order-first order-lg-last mb-3 mb-lg-0">
                 <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
@@ -78,8 +78,8 @@
                 <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
             </div> -->
 
-        </div>
-    </div>
+        <!-- </div> -->
+    <!-- </div> -->
 
 </footer><!-- End Footer -->
 
